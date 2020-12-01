b8702e77d5a28fe103a1fe427fec2c1f72066789
bpkcongli <andrian8367@gmail.com>
2020-12-01 12:37:30 +0700

    Memperbaiki fitur search Home Team dan Away Team

--- input-data-to-database/adding-new-matches/scripts.js
+++ input-data-to-database/adding-new-matches/scripts.js
@@ -1,17 +1,22 @@
 // Base URL untuk kebutuhan fetching data dari API
 const baseUrl = "http://localhost:3000";
 
-const matchCompetitionInput = document.querySelector("#matchCompetitionInput").value;
+/*const matchCompetitionInput = document.querySelector("#matchCompetitionInput").value;
 const matchDateInput = document.querySelector("#matchDateInput").value;
-const matchStatusInput = document.querySelector("#matchStatusInput").value;
+const matchStatusInput = document.querySelector("#matchStatusInput").value;*/
 
 const homeTeamInputBtn = document.querySelector("#homeTeamInputBtn");
-homeTeamInputBtn.addEventListener("click", () => {
+const homeTeamInput = document.querySelector("#homeTeamInput");
+const searchHomeTeam = () => {
     const homeTeamInput = document.querySelector("#homeTeamInput").value;
     searchTeams(homeTeamInput).then(result => {
         const autoCompleteHomeTeam = document.querySelector("#homeTeam .autocomplete-holder");
         autoCompleteHomeTeam.innerHTML = displayAutoCompleteTeams(result);
         autoCompleteHomeTeam.querySelector(".autocomplete").style.display = "block";
+        if (autoCompleteHomeTeam.querySelector(".autocomplete").clientHeight > 200) {
+            autoCompleteHomeTeam.querySelector(".autocomplete").style.height = "200px";
+            autoCompleteHomeTeam.querySelector(".autocomplete").style.overflow = "scroll";
+        }
         autoCompleteHomeTeam.querySelectorAll(".autocomplete-li").forEach(elem => {
             elem.addEventListener("click", () => {
                 const teamChoice = elem.innerText;
@@ -20,14 +25,37 @@ homeTeamInputBtn.addEventListener("click", () => {
             });
         });
     });
+};
+homeTeamInputBtn.addEventListener("click", searchHomeTeam);
+homeTeamInput.addEventListener("keypress", event => {
+    if (event.key === "Enter") searchHomeTeam();
 });
 
 const awayTeamInputBtn = document.querySelector("#awayTeamInputBtn");
-awayTeamInputBtn.addEventListener("click", elem => {
+const awayTeamInput = document.querySelector("#awayTeamInput");
+const searchAwayTeam = () => {
     const awayTeamInput = document.querySelector("#awayTeamInput").value;
-    const result = searchTeams(awayTeamInput);
-    displayAutoCompleteTeams(result);
-});
+    searchTeams(awayTeamInput).then(result => {
+        const autoCompleteAwayTeam = document.querySelector("#awayTeam .autocomplete-holder");
+        autoCompleteAwayTeam.innerHTML = displayAutoCompleteTeams(result);
+        autoCompleteAwayTeam.querySelector(".autocomplete").style.display = "block";
+        if (autoCompleteAwayTeam.querySelector(".autocomplete").clientHeight > 200) {
+            autoCompleteAwayTeam.querySelector(".autocomplete").style.height = "200px";
+            autoCompleteAwayTeam.querySelector(".autocomplete").style.overflow = "scroll";
+        }
+        autoCompleteAwayTeam.querySelectorAll(".autocomplete-li").forEach(elem => {
+            elem.addEventListener("click", () => {
+                const teamChoice = elem.innerText;
+                document.querySelector("#awayTeamInput").value = teamChoice;
+                autoCompleteAwayTeam.querySelector(".autocomplete").style.display = "none";
+            });
+        });
+    });
+};
+awayTeamInputBtn.addEventListener("click", searchAwayTeam);
+awayTeamInput.addEventListener("keypress", event => {
+    if (event.key === "Enter") searchAwayTeam();
+})
 
 const searchTeams = async teamName => {
     const nicknameOfTeams  = [];
