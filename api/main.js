b37bd72def980afaca6dc14eab5fbacce8ef360b
bpkcongli <andrian8367@gmail.com>
2020-12-06 21:16:11 +0700

    Perubahan mayor, penambahan beberapa basic routes

--- api/main.js
+++ api/main.js
@@ -174,6 +174,238 @@ app.get('/api/kickoff/season/:id/standings', (req, res) => {
     });
 });
 
+// POST a new standing ... /api/kickoff/season/:id/standings
+app.post("/api/kickoff/season/:id/standings", (req, res) => {
+    let sql = `INSERT INTO standings (team_id, season_id) VALUES (${req.body.team_id}, ${req.params.id})`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+    });
+    sql = `SELECT * FROM standings ORDER BY id DESC LIMIT 1`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+        else res.json({
+            status: 200,
+            err: null,
+            message: "A new team standing is added successfully.",
+            response: results[0]
+        });
+    });
+});
+
+// PUT a team standing ... /api/kickoff/season/:id/standings
+app.put("/api/kickoff/season/:id/standings", (req, res) => {
+    let sql = `UPDATE standings SET plays=${req.body.plays}, wins=${req.body.wins}, draws=${req.body.draws}, losses=${req.body.losses}, goals_against=${req.body.goals_against}, goals_for=${req.body.goals_for}, goals_difference=${req.body.goals_difference}, points=${req.body.points}, last_five_matches='${req.body_last_five_matches}' WHERE team_id=${req.body.team_id} AND season_id=${req.params.id}`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+    });
+    sql = `SELECT * FROM standings WHERE team_id=${req.body.team_id} AND season_id=${req.params.id}`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+        else res.json({
+            status: 200,
+            err: null,
+            message: "Standings has been successfully updated.",
+            response: results[0]
+        });
+    });
+});
+
+// GET all goal scores on a season ... /api/kickoff/season/:id/goals
+app.get("/api/kickoff/season/:id/goals", (req, res) => {
+    let sql = `SELECT * FROM goals_on_seasons WHERE season_id=${req.params.id} ORDER BY goals_scored DESC`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+        else res.json({
+            status: 200,
+            err: null,
+            response: results
+        });
+    });
+});
+
+// POST a new goal scores on a season ... /api/kickoff/season/:id/goals
+app.post("/api/kickoff/season/:id/goals", (req, res) => {
+    let sql = `INSERT INTO goals_on_seasons (season_id, player_id, goals_on_scored) VALUES (${req.params.id}, ${req.body.player_id}, ${req.body.goals_on_scored})`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+    });
+    sql = `SELECT * FROM goals_on_seasons ORDER BY id DESC LIMIT 1`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+        else res.json({
+            status: 200,
+            err: null,
+            message: "A new goals on seasons is added successfully.",
+            response: results[0]
+        });
+    });
+});
+
+// PUT a goal scores on a season ... /api/kickoff/season/:id/goals
+app.put("/api/kickoff/season/:id/goals", (req, res) => {
+    let sql = `UPDATE goals_on_seasons SET goals_on_scored=${req.body.goals_on_scored} WHERE season_id=${req.params.id} AND player_id=${req.body.player_id}`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+    });
+    sql = `SELECT * FROM goals_on_seasons WHERE season_id=${req.params.id} AND player_id=${req.body.player_id}`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+        else res.json({
+            status: 200,
+            err: null,
+            message: "Goals on seasons has been successfully updated.",
+            response: results[0]
+        });
+    });
+});
+
+// GET all assists created on a season ... /api/kickoff/season/:id/assists
+app.get("/api/kickoff/season/:id/assists", (req, res) => {
+    let sql = `SELECT * FROM assists_on_seasons WHERE season_id=${req.params.id} ORDER BY assists_created DESC`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+        else res.json({
+            status: 200,
+            err: null,
+            response: results
+        });
+    });
+});
+
+// POST a new assists created on a season ... /api/kickoff/season/:id/assists
+app.post("/api/kickoff/season/:id/assists", (req, res) => {
+    let sql = `INSERT INTO assists_on_seasons (season_id, player_id, assists_created) VALUES (${req.params.id}, ${req.body.player_id}, ${req.body.assists_created})`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+    });
+    sql = `SELECT * FROM assists_on_seasons ORDER BY id DESC LIMIT 1`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+        else res.json({
+            status: 200,
+            err: null,
+            message: "A new assists on seasons is added successfully.",
+            response: results[0]
+        });
+    });
+});
+
+// PUT a assists created on a season ... /api/kickoff/season/:id/assists
+app.put("/api/kickoff/season/:id/assists", (req, res) => {
+    let sql = `UPDATE assists_on_seasons SET assists_created=${req.body.assists_created} WHERE season_id=${req.params.id} AND player_id=${req.body.player_id}`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+    });
+    sql = `SELECT * FROM assists_on_seasons WHERE season_id=${req.params.id} AND player_id=${req.body.player_id}`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+        else res.json({
+            status: 200,
+            err: null,
+            message: "Assists on seasons has been successfully updated.",
+            response: results[0]
+        });
+    });
+});
+
+// GET all red cards obtained by players on a season ... /api/kickoff/season/:id/red-cards
+app.get("/api/kickoff/season/:id/red-cards", (req, res) => {
+    let sql = `SELECT * FROM red_cards_on_seasons WHERE season_id=${req.params.id} ORDER BY red_cards_obtained DESC`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+        else res.json({
+            status: 200,
+            err: null,
+            response: results
+        });
+    });
+});
+
+// POST a red cards obtained by players on a season ... /api/kickoff/season/:id/red-cards
+app.post("/api/kickoff/season/:id/red-cards", (req, res) => {
+    let sql = `INSERT INTO red_cards_on_seasons (season_id, player_id, red_cards_obtained) VALUES (${req.params.id}, ${req.body.player_id}, ${req.body.red_cards_obtained})`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+    });
+    sql = `SELECT * FROM red_cards_on_seasons ORDER BY id DESC LIMIT 1`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+        else res.json({
+            status: 200,
+            err: null,
+            message: "A new red cards on seasons is added successfully.",
+            response: results[0]
+        });
+    });
+});
+
+// PUT a red cards obtained by players on a season ... /api/kickoff/season/:id/red-cards
+app.put("/api/kickoff/season/:id/red-cards", (req, res) => {
+    let sql = `UPDATE red_cards_on_seasons SET red_cards_obtained=${req.body.red_cards_obtained} WHERE season_id=${req.params.id} AND player_id=${req.body.player_id}`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+    });
+    sql = `SELECT * FROM red_cards_on_seasons WHERE season_id=${req.params.id} AND player_id=${req.body.player_id}`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+        else res.json({
+            status: 200,
+            err: null,
+            message: "Red cards on seasons has been successfully updated.",
+            response: results[0]
+        });
+    });
+});
+
+// GET all yellow cards obtained by players on a season ... /api/kickoff/season/:id/yellow-cards
+app.get("/api/kickoff/season/:id/yellow-cards", (req, res) => {
+    let sql = `SELECT * FROM yellow_cards_on_seasons WHERE season_id=${req.params.id} ORDER BY yellow_cards_obtained DESC`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+        else res.json({
+            status: 200,
+            err: null,
+            response: results
+        });
+    });
+});
+
+// POST a yellow cards obtained by players on a season ... /api/kickoff/season/:id/yellow-cards
+app.post("/api/kickoff/season/:id/yellow-cards", (req, res) => {
+    let sql = `INSERT INTO yellow_cards_on_seasons (season_id, player_id, yellow_cards_obtained) VALUES (${req.params.id}, ${req.body.player_id}, ${req.body.yellow_cards_obtained})`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+    });
+    sql = `SELECT * FROM yellow_cards_on_seasons ORDER BY id DESC LIMIT 1`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+        else res.json({
+            status: 200,
+            err: null,
+            message: "A new yellow cards on seasons is added successfully.",
+            response: results[0]
+        });
+    });
+});
+
+// PUT a yellow cards obtained by players on a season ... /api/kickoff/season/:id/yellow-cards
+app.put("/api/kickoff/season/:id/yellow-cards", (req, res) => {
+    let sql = `UPDATE yellow_cards_on_seasons SET yellow_cards_obtained=${req.body.yellow_cards_obtained} WHERE season_id=${req.params.id} AND player_id=${req.body.player_id}`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+    });
+    sql = `SELECT * FROM yellow_cards_on_seasons WHERE season_id=${req.params.id} AND player_id=${req.body.player_id}`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+        else res.json({
+            status: 200,
+            err: null,
+            message: "Yellow cards on seasons has been successfully updated.",
+            response: results[0]
+        });
+    });
+});
+
 // GET all teams ... /api/kickoff/teams
 app.get('/api/kickoff/teams', (req, res) => {
     const sql = `SELECT * FROM teams`;
@@ -292,6 +524,32 @@ app.post('/api/kickoff/coaches', (req, res) => {
     });
 });
 
+// GET a specific match ... /api/kickoff/match/:id
+app.get("/api/kickoff/match/:id", (req, res) => {
+    let sql = `SELECT * FROM matches WHERE id=${req.params.id}`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+        else res.json({
+            status: 200,
+            err: null,
+            response: results[0]
+        });
+    });
+});
+
+// GET all matches on specific season competition ... /api/kickoff/season/:id/matches
+app.get("/api/kickoff/season/:id/matches", (req, res) => {
+    let sql = `SELECT * FROM matches WHERE season_id=${req.params.id} ORDER BY time_date`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+        else res.json({
+            status: 200,
+            err: null,
+            response: results
+        });
+    });
+});
+
 // POST a new match ... /api/kickoff/matches
 app.post("/api/kickoff/matches", (req, res) => {
     let sql = `INSERT INTO matches (season_id, time_date, status, matchday, home_team_id, away_team_id, score_home_team_half_time, score_away_team_half_time, score_home_team_full_time, score_away_team_full_time, stadium) `;
@@ -311,28 +569,23 @@ app.post("/api/kickoff/matches", (req, res) => {
     });
 });
 
-// PUT a team standing ... /api/kickoff/season/:id/standings
-app.put("/api/kickoff/season/:id/standings", (req, res) => {
-    let sql = `UPDATE standings SET plays=${req.body.plays}, wins=${req.body.wins}, draws=${req.body.draws}, losses=${req.body.losses}, goals_against=${req.body.goals_against}, goals_for=${req.body.goals_for}, goals_difference=${req.body.goals_difference}, points=${req.body.points}, last_five_matches='${req.body_last_five_matches}' WHERE team_id=${req.body.team_id}`;
-    conn.query(sql, (err, results) => {
-        if (err) throw err;
-    });
-    sql = `SELECT * FROM standings WHERE team_id=${req.body.team_id}`;
+// GET a specific match stats ... /api/kickoff/match/:id/stats
+app.get("/api/kickoff/match/:id/stats", (req, res) => {
+    let sql = `SELECT * FROM match_stats WHERE match_id=${req.params.id}`;
     conn.query(sql, (err, results) => {
         if (err) throw err;
         else res.json({
             status: 200,
             err: null,
-            message: "Standings has been successfully updated.",
             response: results[0]
         });
     });
 });
 
-// POST a new match stats ... /api/kickoff/match-stats
-app.post("/api/kickoff/match-stats", (req, res) => {
+// POST a new match stats ... /api/kickoff/match/:id/stats
+app.post("/api/kickoff/match/:id/stats", (req, res) => {
     let sql = `INSERT INTO match_stats (match_id, home_team_shots, away_team_shots, home_team_shots_on_target, away_team_shots_on_target, home_team_possession, away_team_possession, home_team_passes, away_team_passes, home_team_pass_accuracy, away_team_pass_accuracy, home_team_fouls, away_team_fouls, home_team_yellow_cards, away_team_yellow_cards, home_team_red_cards, away_team_red_cards, home_team_offsides, away_team_offsides, home_team_corners, away_team_corners) `;
-    sql += `VALUES (${req.body.match_id}, ${req.body.home_team_shots}, ${req.body.away_team_shots}, ${req.body.home_team_shots_on_target}, ${req.body.away_team_shots_on_target}, ${req.body.home_team_possession}, ${req.body.away_team_possession}, ${req.body.home_team_passes}, ${req.body.away_team_passes}, ${req.body.home_team_pass_accuracy}, ${req.body.away_team_pass_accuracy}, ${req.body.home_team_fouls}, ${req.body.away_team_fouls}, ${req.body.home_team_yellow_cards}, ${req.body.away_team_yellow_cards}, ${req.body.home_team_red_cards}, ${req.body.away_team_red_cards}, ${req.body.home_team_offsides}, ${req.body.away_team_offsides}, ${req.body.home_team_corners}, ${req.body.away_team_corners})`;
+    sql += `VALUES (${req.params.id}, ${req.body.home_team_shots}, ${req.body.away_team_shots}, ${req.body.home_team_shots_on_target}, ${req.body.away_team_shots_on_target}, ${req.body.home_team_possession}, ${req.body.away_team_possession}, ${req.body.home_team_passes}, ${req.body.away_team_passes}, ${req.body.home_team_pass_accuracy}, ${req.body.away_team_pass_accuracy}, ${req.body.home_team_fouls}, ${req.body.away_team_fouls}, ${req.body.home_team_yellow_cards}, ${req.body.away_team_yellow_cards}, ${req.body.home_team_red_cards}, ${req.body.away_team_red_cards}, ${req.body.home_team_offsides}, ${req.body.away_team_offsides}, ${req.body.home_team_corners}, ${req.body.away_team_corners})`;
     conn.query(sql, (err, results) => {
         if (err) throw err;
     });
@@ -348,10 +601,23 @@ app.post("/api/kickoff/match-stats", (req, res) => {
     });
 });
 
-// POST a new goals on matches ... /api/kickoff/goals-on-matches
-app.post("/api/kickoff/goals-on-matches", (req, res) => {
-    let sql = `INSERT INTO goals_on_matches (match_stats_id, player_id, minutes, status_team, status_goals) `;
-    sql += `VALUES (${req.body.match_stats_id}, ${req.body.player_id}, ${req.body.minutes}, '${req.body.status_team}', '${req.body.status_goals}')`;
+// GET all goals scored in specific match ... /api/kickoff/match/:id/goals
+app.get("/api/kickoff/match/:id/goals", (req, res) => {
+    let sql = `SELECT * FROM goals_on_matches WHERE match_id=${req.params.id} ORDER BY minutes`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+        else res.json({
+            status: 200,
+            err: null,
+            response: results
+        });
+    });
+});
+
+// POST a new goals on matches ... /api/kickoff/match/:id/goals
+app.post("/api/kickoff/match/:id/goals", (req, res) => {
+    let sql = `INSERT INTO goals_on_matches (match_id, player_id, minutes, status_team, status_goals) `;
+    sql += `VALUES (${req.params.id}, ${req.body.player_id}, ${req.body.minutes}, '${req.body.status_team}', '${req.body.status_goals}')`;
     conn.query(sql, (err, results) => {
         if (err) throw err;
     });
@@ -362,6 +628,38 @@ app.post("/api/kickoff/goals-on-matches", (req, res) => {
             status: 200,
             err: null,
             message: "A new goals on matches is added successfully.",
+            response: results[0]
+        });
+    });
+});
+
+// GET all red cards obtained in specific match ... /api/kickoff/match/:id/red-cards
+app.get("/api/kickoff/match/:id/red-cards", (req, res) => {
+    let sql = `SELECT * FROM red_cards_on_matches WHERE match_id=${req.params.id}`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+        else res.json({
+            status: 200,
+            err: null,
+            response: results
+        });
+    });
+});
+
+// POST a new red cards on matches ... /api/kickoff/match/:id/red-cards
+app.post("/api/kickoff/match/:id/red-cards", (req, res) => {
+    let sql = `INSERT INTO red_cards_on_matches (match_id, player_id, status_team) `;
+    sql += `VALUES (${req.params.id}, ${req.body.player_id}, '${req.body.status_team}')`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+    });
+    sql = `SELECT * FROM red_cards_on_matches ORDER BY id DESC LIMIT 1`;
+    conn.query(sql, (err, results) => {
+        if (err) throw err;
+        else res.json({
+            status: 200,
+            err: null,
+            message: "A new red cards on matches is added successfully.",
             response: results[0]
         });
     });
